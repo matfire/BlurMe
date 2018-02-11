6c85a3d48d392b781f22ece10e21b01af510e70a
Matteo <matteo@localhost.localdomain>
2018-02-11 15:47:22 +0100

    fixed minor mistakes

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ video = cv2.VideoCapture(0)
 while True:
     # Capture frame-by-frame
     ret, im = video.read()
-    face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
-    eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
     # Our operations on the frame come here
-    grey =  cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    grey =  cv2.cvtColor(img, cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
-        cv.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = im[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in eyes:
-            cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
     # Display the resulting frame
     cv2.imshow("BlurMe", im)
     if cv2.waitKey(10) == 27:
