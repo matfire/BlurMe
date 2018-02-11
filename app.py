63066b1766ac0be97b3444fd52e65e81199ce170
Matteo <matteo@localhost.localdomain>
2018-02-11 15:39:50 +0100

    configure face capture using webcam and greyscale conversion

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-import cv2 as cv
+import cv2
+import numpy as np
 
+video = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, im = video.read()
+
+    # Our operations on the frame come here
+    ret, im2 = cv2.threshold(cv2.cvtColor(im, cv2.COLOR_BGR2GRAY), 100, 255, cv2.THRESH_BINARY)
+
+    # Display the resulting frame
+    cv2.imshow("BlurMe", im2)
+    if cv2.waitKey(10) == 27:
+        break
 
