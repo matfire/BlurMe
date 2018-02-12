0023142c89c09bab9fc669697790accc3bbf1a29
Matteo Gassend <matteo.gassend@epitech.eu>
2018-02-12 18:28:04 +0100

    added xml files required to process images

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import cv2
 import numpy as np
 
@@ -9,17 +11,16 @@ while True:
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
     # Our operations on the frame come here
-    grey =  cv2.cvtColor(im, cv.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(im, (x, y), (x + w, y + h), (255, 0, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = im[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
     # Display the resulting frame
     cv2.imshow("BlurMe", im)
     if cv2.waitKey(10) == 27:
         break
-
